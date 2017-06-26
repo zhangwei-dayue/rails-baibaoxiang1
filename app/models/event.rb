90ba2b9338676cda778788f728e5bff3dca7abf8
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-06-26 23:07:09 +0800

    用户可以自定义网址

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,9 @@
 class Event < ApplicationRecord
 
- validates_presence_of :name
- before_validation :generate_friendly_id, :on => :create
+  validates_presence_of :name, :friendly_id
+  validates_uniqueness_of :friendly_id
+  validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
+  before_validation :generate_friendly_id, :on => :create
 
   def to_param
     self.friendly_id
