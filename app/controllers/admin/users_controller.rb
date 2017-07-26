98bc16dedb47de9ccde6204059cc7221b378fcac
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-07-26 22:54:48 +0800

    用户多选group

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,7 @@
 class Admin::UsersController < AdminController
 
   def index
-    @users = User.all
+    @users = User.includes(:groups).all
   end
 
   def edit
@@ -21,6 +21,6 @@ class Admin::UsersController < AdminController
   protected
 
   def user_params
-    params.require(:user).permit(:email)
+    params.require(:user).permit(:email, :group_ids => [])
   end
 end
