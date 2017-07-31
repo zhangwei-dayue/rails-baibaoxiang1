0cc3c563c16dc217e888088af20a43ec96611843
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-08-01 07:01:28 +0800

    增加自定义验证

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -6,6 +6,7 @@ class Registration < ApplicationRecord
   attr_accessor :current_step
   validates_presence_of :name, :email, :cellphone, :if => :should_validate_basic_data?
   validates_presence_of :name, :email, :cellphone, :bio, :if => :should_validate_all_data?
+  validate :check_event_status, :on => :create
 
   belongs_to :event
   belongs_to :ticket
@@ -29,5 +30,11 @@ class Registration < ApplicationRecord
 
   def should_validate_all_data?
     current_step == 3 || status == "confirmed"
+  end
+
+  def check_event_status
+    if self.event.status == "draft"
+      errors.add(:base, "活动尚未开放报名")
+    end
   end
 end
