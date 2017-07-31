d36a815e11db34619d3c24d51ca452357078d9d4
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-08-01 06:09:01 +0800

    增加条件表单验证

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -3,6 +3,9 @@ class Registration < ApplicationRecord
   STATUS = ["pending", "confirmed"]
   validates_inclusion_of :status, :in => STATUS
   validates_presence_of :status, :ticket_id
+  attr_accessor :current_step
+  validates_presence_of :name, :email, :cellphone, :if => :should_validate_basic_data?
+  validates_presence_of :name, :email, :cellphone, :bio, :if => :should_validate_all_data?
 
   belongs_to :event
   belongs_to :ticket
@@ -18,5 +21,13 @@ class Registration < ApplicationRecord
 
   def generate_uuid
     self.uuid = SecureRandom.uuid
+  end
+
+  def should_validate_basic_data?
+    current_step == 2
+  end
+
+  def should_validate_all_data?
+    current_step == 3 || status == "confirmed"
   end
 end
