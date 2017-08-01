c1b24b538933645295afbb6c4b444691fb891bf8
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-08-01 20:49:29 +0800

    输入报名编号实现筛选

--- app/controllers/admin/event_registrations_controller.rb
+++ app/controllers/admin/event_registrations_controller.rb
@@ -12,6 +12,10 @@ class Admin::EventRegistrationsController < AdminController
       @registrations = @registrations.where("created_at <= ?", Date.parse(params[:end_on]).end_of_day)
     end
 
+    if params[:registration_id].present?
+      @registrations = @registrations.where( :id => params[:registration_id].split(","))
+    end
+
     if Array(params[:statuses]).any?
       @registrations = @registrations.by_status(params[:statuses])
     end
