58bca501c6cea305d838c19aa882268cf8f462ce
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-08-02 19:36:14 +0800

    资料汇出为number或者excel

--- app/controllers/admin/event_registrations_controller.rb
+++ app/controllers/admin/event_registrations_controller.rb
@@ -1,3 +1,4 @@
+require 'csv'
 class Admin::EventRegistrationsController < AdminController
   before_action :find_event
 
@@ -31,6 +32,20 @@ class Admin::EventRegistrationsController < AdminController
 
     if params[:ticket_id].present?
       @registrations = @registrations.by_ticket(params[:ticket_id])
+    end
+
+    respond_to do |format|
+      format.html
+      format.csv {
+        @registrations = @registrations.reorder("id ASC")
+        csv_string = CSV.generate do |csv|
+          csv << ["报名ID", "票种", "姓名", "状态", "Email", "报名时间"]
+          @registrations.each do |r|
+            csv << [r.id, r.ticket.name, r.name, t(r.status, :scope => "registration.status"), r.email, r.created_at]
+          end
+        end
+        send_data csv_string, :filename => "#{@event.friendly_id}-registrations-#{Time.now.to_s(:number)}.csv"
+      }
     end
   end
 
