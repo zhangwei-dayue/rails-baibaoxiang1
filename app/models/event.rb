19353e00534d5ab3c78a11d9d40bf13fca217e0a
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-08-04 06:17:16 +0800

    显示汇入CSVindex和详细信息

--- app/models/event.rb
+++ app/models/event.rb
@@ -15,6 +15,7 @@ class Event < ApplicationRecord
   belongs_to :category, :optional => true
   has_many :tickets, :dependent => :destroy
   has_many :registrations, :dependent => :destroy
+  has_many :registration_imports, :dependent => :destroy
   accepts_nested_attributes_for :tickets, :allow_destroy => true, :reject_if => :all_blank
 
   has_many :attachments, :class_name => "EventAttachment", :dependent => :destroy
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root "events#index"
     resources :events do
+      resources :registration_imports
       resources :registrations, :controller => "event_registrations" do
         collection do
           post :import
